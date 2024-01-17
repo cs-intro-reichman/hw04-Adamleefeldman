af25ce6adb700aa18626146ef9234568d010f0c2
Adam Feldman <adamfeldman@Adams-MacBook-Air.local>
2024-01-17 11:11:17 +0200

    camel case fix

--- StringOps.java
+++ StringOps.java
@@ -22,7 +22,7 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        System.out.println(allIndexOf("Hello world", 'l'));
+        System.out.println(camelCase(" two  wordS"));
         
     }
 
@@ -46,40 +46,37 @@ public class StringOps {
 
     public static String camelCase (String string) {
         String newString = "";
-        String stringOut = "";
         boolean isSpace = false;
 
         for (int i = 0; i < string.length(); i++){
+            char currentChar = string.charAt(i);
             
-            if (string.charAt(i) == 32){
+            if (currentChar == ' '){
                 isSpace = true;
             }else if (isSpace){
-                if (string.charAt(i) >= 97 && string.charAt(i) <= 122){
-                    newString = newString + (char)(string.charAt(i) - 32);
+                if (currentChar >= 'a' && currentChar <= 'z'){
+                    newString = newString + (char)(currentChar - 'a' + 'A');
                     isSpace = false;
-                }else if (string.charAt(i) >= 65 && string.charAt(i) <= 90){
-                    newString = newString + (char)(string.charAt(i));
+                }else if (string.charAt(i) >= 'A' && string.charAt(i) <= 'Z'){
+                    newString = newString + currentChar;
                     isSpace = false;
-                }else if (string.charAt(i) == 32){
-                    isSpace = true;
+                }else {
+                    newString = newString + currentChar;
                 }
-            }else if(string.charAt(i) != 32 && (string.charAt(i) >= 65 && string.charAt(i) <= 90)){
-                newString = newString + (char)(string.charAt(i) + 32);
+            }else if(currentChar != ' ' && (currentChar >= 'A' && currentChar <= 'Z')){
+                newString = newString + (char)(currentChar - 'A' + 'a');
                 isSpace = false;
             }else{
-                newString = newString + (char)(string.charAt(i));
+                newString = newString + currentChar;
                 isSpace = false;
             }
         }
 
-        if (newString.charAt(0) >= 65 && newString.charAt(0) <= 90){
-            newString = newString + (char)(newString.charAt(0) + 32);
-            stringOut = newString + newString.substring(1);     
-        }else{
-            stringOut = newString;
+        if (newString.length() > 0 && newString.charAt(0) >= 'A' && newString.charAt(0) <= 'Z'){
+            newString = (char)(newString.charAt(0) - 'A' + 'a') + newString.substring(1);
         }
 
-        return stringOut;
+        return newString;
     }
     
 
